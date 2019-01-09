83f258928e0d947f9f9e3437d7b6915b2e6a850d
by <983313620@qq.com>
2019-01-09 17:07:46 +0800

    获取用户id

--- src/main/java/com/qfedu/controller/UserController.java
+++ src/main/java/com/qfedu/controller/UserController.java
@@ -12,12 +12,17 @@ import org.apache.shiro.authc.UsernamePasswordToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.apache.shiro.subject.Subject;
 
+import javax.servlet.http.HttpSession;
 import java.io.UnsupportedEncodingException;
 import java.util.Base64;
+import java.util.Collection;
+import java.util.Enumeration;
+import java.util.List;
 
 @Api(produces="登陆注册接口",value="接口文档")
 @Controller
@@ -56,11 +61,31 @@ public class UserController {
             //2、设置登录的Token
             UsernamePasswordToken token=new UsernamePasswordToken(name, password);
             //3、存储数据到Session
-            subject.getSession().setAttribute("user", ro.getData());
+            subject.getSession().setAttribute(name, ro.getData());
             //4、登录认证
             subject.login(token);
         }
         return ro;
     }
+
+    @ApiOperation(value="得到用户")
+    @GetMapping("getUserId.do")
+    @ResponseBody
+    @CrossOrigin
+    public ResultVo getUserId() {
+        Subject subject = SecurityUtils.getSubject();
+        Collection<Object> keys = subject.getSession().getAttributeKeys();
+        User user = new User();
+        if (!keys.isEmpty() && keys.size() != 0) {
+            for (Object key : keys) {
+                User user2 = (User) subject.getSession().getAttribute(key);
+                user.setUserId(user2.getUserId());
+                user.setUserName(user2.getUserName());
+                return ResultVo.setOK(user);
+            }
+        }
+        return ResultVo.setERROR();
+    }
+
 
 }
